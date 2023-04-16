c20e18791bfeaa90676adf7db548d3dccdb6fa08
柯仁傑 <@DESKTOP-34DDU9R>
2023-04-16 16:23:54 +0800

    加入專案檔案。

---
+++ homework16/homework16.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s1, s2;
+    getline(cin, s1);
+    getline(cin, s2);
+    int count = 0;
+    size_t pos = s2.find(s1);
+    while (pos != string::npos) {
+        count++;
+        pos = s2.find(s1, pos + 1);
+    }
+    cout << count << endl;
+    return 0;
+}
